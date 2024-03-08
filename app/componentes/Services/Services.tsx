07c58b507e3e774fefd86b80b46c0bff35e3bd6c
danielmayo <daniielmayo.mayo.com>
2024-03-08 17:53:06 -0500

    create sesion services

--- app/componentes/Services/Services.tsx
+++ app/componentes/Services/Services.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import CardServices from "./CardServices";
-import styles from "./Services.module.css";
+import { Slider } from "./Slider";
+// import { Slider } from "./Slider";
+// import { Slider2 } from "./Slider2";
 
 export const Services = () => {
   return (
-    <div className={styles["container-services"]}>
-      <CardServices />
-      <CardServices />
-      <CardServices />
-      <CardServices />
+    <div>
+      <div className="w-[100%] flex flex-col justify-center">
+        <h2 className="text-[var(--greenDevtools)] font-bold text-2xl self-center mt-8">
+          Nuestros Servicios
+        </h2>
+        <div
+        // className={`flex flex-col md:grid grid-cols-3 gap-4 w-[60%] items-center self-center`}
+        >
+          {/* <CardServices />
+        <CardServices />
+        <CardServices />
+      <CardServices /> */}
+          <Slider />
+        </div>
+        {/* <Slider2 /> */}
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,20 @@ import { nextui } from "@nextui-org/theme";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
     "/app/componentes/**",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [nextui(), require("flowbite/plugin")],
 };
