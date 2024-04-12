a7cada8137ee8d261608a9858238cbaa6ef6186d
william serna <wserna880@gmail.com>
2024-04-12 10:15:03 -0500

    index has been added to the team component map

--- app/componentes/Team/Team.tsx
+++ app/componentes/Team/Team.tsx
@@ -44,8 +44,8 @@ export const Team = () => {
             <h1 className="text-white text-3xl font-semibold ml-5">Nuestro equipo</h1>
             <hr className="border-t-4 border-green-l mb-3 ml-5 w-[45%]"/>
             <div className="rounded-[30px] grid sm:grid-cols-3 gap-3 p-10 backdrop-blur-md bg-white/30 ">
-                {team.map((employee) => (
-                    <div className="">
+                {team.map((employee, index) => (
+                    <div className="" key={index}>
                         <Image src={employee?.image} alt={"employee-img"} className="h-40 object-cover border-2 rounded-[30px]  border-green-l w-full" width={100} height={100} />
                         <h1 className="text-xs text-white text-center font-semibold mt-4">{employee?.name}</h1>
                         <h2 className="text-xs text-green-l text-center font-semibold mb-2">{employee?.job}</h2>
