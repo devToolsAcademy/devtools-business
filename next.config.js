573df9280f673e5980c5141ace17839740a68fde
william serna <wserna880@gmail.com>
2024-05-16 12:57:42 -0500

    update next configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['dgpi9g6k6hezi.cloudfront.net'],
     remotePatterns: [
       { protocol: "https", hostname: "nextui-docs-v2.vercel.app" },
     ],
