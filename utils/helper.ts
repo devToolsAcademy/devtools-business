a804ef0cacd19664115d8eb6bf93c915cace0bef
william serna <wserna880@gmail.com>
2024-05-21 17:16:07 -0500

    changing phone number

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,9 +1,9 @@
 export const handleWhatsappLink = () => {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
       // El usuario está en un dispositivo móvil, abre el enlace de WhatsApp para móviles
-      window.location.href = `https://wa.me/3116978993`;
+      window.location.href = `https://wa.me/3127570197`;
     } else {
       // El usuario está en una computadora de escritorio, abre el enlace de WhatsApp Web
-      window.open(`https://web.whatsapp.com/send?phone=3116978993`);
+      window.open(`https://web.whatsapp.com/send?phone=3127570197`);
     }
   };
